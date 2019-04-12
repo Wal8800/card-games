73c5faf69eb4e12bc4b968de7272bd6bc4e99106
John Law <johnlaw1234@hotmail.com>
2019-04-13 09:02:38 +1200

    Use one hot encoding for cards

--- bigtwo/valid_cards_game.py
+++ bigtwo/valid_cards_game.py
@@ -2,6 +2,14 @@ import numpy as np
 from playingcards.card import Card
 from bigtwo import BigTwo
 from gym import spaces
+import random
+from sklearn.preprocessing import OneHotEncoder
+
+suit_encoder = OneHotEncoder(sparse=False, categories='auto')
+temp = np.array(range(4)).reshape(4, 1)
+suit_one_hot = suit_encoder.fit_transform(temp)
+rank_encoder = OneHotEncoder(sparse=False, categories='auto')
+rank_one_hot = rank_encoder.fit_transform(np.array(range(13)).reshape(13, 1))
 
 
 def pick_cards_from_hand(raw_action, hand):
@@ -13,10 +21,54 @@ def pick_cards_from_hand(raw_action, hand):
     return action
 
 
+def to_rank_suit_obs(card_numbers):
+    suit_rank_obs = np.array([])
+    for card in card_numbers:
+        rank = card % 13
+        suit = card // 13
+
+        suit_rank_obs = np.concatenate([suit_rank_obs, [suit, rank]])
+
+    return suit_rank_obs
+
+
+def to_one_hot_encoding(card_numbers):
+    one_hot_obs = np.array([])
+    for card in card_numbers:
+        rank = card % 13
+        suit = card // 13
+
+        suit_array = suit_one_hot[suit]
+        rank_array = rank_one_hot[rank]
+        one_hot_obs = np.concatenate([one_hot_obs, suit_array, rank_array])
+
+    return one_hot_obs
+
+
+def from_one_hot_encoding(obs):
+    suit_arr_list = [obs[x:x + 4] for x in range(0, 221, 17)]
+    rank_arr_list = [obs[x:x + 13] for x in range(4, 221, 17)]
+    suit_rank_list = list(
+        zip(
+            suit_encoder.inverse_transform(suit_arr_list).flatten(),
+            rank_encoder.inverse_transform(rank_arr_list).flatten()
+        )
+    )
+    hand = []
+    for pair in suit_rank_list:
+        hand.append((pair[0] * 13) + pair[1])
+
+    return [Card.from_number(x) for x in hand]
+
+
+def random_hand():
+    return random.sample(range(52), 13)
+
+
 class ValidCardGame:
     def __init__(self):
         self.current_hand = None
-        self.observation_space = spaces.Box(low=-1, high=51, shape=(13,), dtype=np.int32)
+        self.observation_space = spaces.Box(low=0, high=51, shape=(13,), dtype=np.int32)
         self.action_space = spaces.MultiBinary(13)
         self.past_result = []
 
@@ -26,18 +78,53 @@ class ValidCardGame:
         valid_card = False
         if len(cards) > 0:
             valid_card = BigTwo.is_valid_card_combination(cards)
-        reward = len(cards)*len(cards) if valid_card else 0
+
+        reward = 0
+        if len(cards) == 1 or len(cards) == 2 or len(cards) == 5:
+            reward += len(cards) * len(cards)
+
+        if valid_card and len(cards) > 1:
+            reward += len(cards) * len(cards)
 
         self.past_result.append((self.current_hand, cards, reward))
 
-        self.current_hand = self.random_hand()
-        return self.random_hand(), reward, reward == 0, {}
+        self.current_hand = random_hand()
+        return self.current_hand, reward, reward == 0, {}
 
     def reset(self):
-        self.current_hand = ValidCardGame.random_hand()
+        self.current_hand = random_hand()
         self.past_result = []
+
         return self.current_hand
 
-    @staticmethod
-    def random_hand():
-        return np.random.randint(52, size=13)
+
+class ValidCardGameAlt:
+    def __init__(self):
+        self.current_hand = None
+        # 13 cards * 4 suit * 13 type of cards
+        self.observation_space = spaces.Box(low=0, high=12, shape=(221,), dtype=np.int32)
+        self.action_space = spaces.MultiBinary(13)
+        self.past_result = []
+
+    def step(self, action):
+        hand = [Card.from_number(x) for x in self.current_hand]
+        cards = pick_cards_from_hand(action, hand)
+        valid_card = False
+        if len(cards) > 0:
+            valid_card = BigTwo.is_valid_card_combination(cards)
+
+        reward = len(cards) * len(cards) if valid_card and len(cards) > 1 else 0
+
+        self.past_result.append((self.current_hand, cards, reward))
+
+        self.current_hand = random_hand()
+        self.current_hand.sort()
+        obs = to_one_hot_encoding(self.current_hand)
+        return obs, reward, reward <= 0, {}
+
+    def reset(self):
+        self.current_hand = random_hand()
+        self.current_hand.sort()
+        self.past_result = []
+
+        return to_one_hot_encoding(self.current_hand)
--- gamerunner/ppo_valid_cards_picker.py
+++ gamerunner/ppo_valid_cards_picker.py
@@ -1,33 +1,114 @@
 from stable_baselines.common.policies import MlpPolicy
-from stable_baselines.common.vec_env import DummyVecEnv
-from stable_baselines import PPO2
-from bigtwo.valid_cards_game import ValidCardGame, pick_cards_from_hand
+from stable_baselines.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines import PPO2, A2C
+from bigtwo.valid_cards_game import ValidCardGame, pick_cards_from_hand, ValidCardGameAlt, from_one_hot_encoding
 from playingcards.card import Card
+import numpy as np
+import tensorflow as tf
 
 
-base_env = ValidCardGame()
+def evaluate(model, env):
+    obs = env.reset()
+    total_rewards = 0
+    for i in range(20):
+        action, _states = model.predict(obs)
+        new_obs, rewards, dones, info = env.step(action)
+
+        if rewards[0] > 0:
+            hand = [Card.from_number(x) for x in obs[0]]
+            # obs_list = obs[0]
+            # hand = []
+            # for j in range(0, 26, 2):
+            #     hand.append((obs_list[j] * 13) + obs_list[j+1])
+            #
+            # hand = [Card.from_number(x) for x in hand]
+
+            cards = pick_cards_from_hand(action[0], hand)
+            hand_str = Card.display_cards_string(obs[0])
+            # hand_str = ' '.join(str(x) for x in hand)
+            card_str = ' '.join(str(x) for x in cards)
+            print("hand: {:s} selected: {:s} reward: {:f}".format(hand_str, card_str, rewards[0]))
+
+        obs = new_obs
+        total_rewards += rewards[0]
+        # env.render()
+
+    print("total rewards: ", total_rewards)
+
+
+def evaluate_alt(model, env):
+    obs = env.reset()
+    total_rewards = 0
+    for i in range(20):
+        action, _states = model.predict(obs)
+        new_obs, rewards, dones, info = env.step(action)
+
+        if rewards > 0:
+            hand = from_one_hot_encoding(obs[0])
+            cards = pick_cards_from_hand(action[0], hand)
+            hand_str = ' '.join(str(x) for x in hand)
+            card_str = ' '.join(str(x) for x in cards)
+            print("hand: {:s} selected: {:s} reward: {:f}".format(hand_str, card_str, rewards[0]))
+
+        obs = new_obs
+        total_rewards += rewards[0]
+        # env.render()
+
+    print("total rewards: ", total_rewards)
+
+
+def evaluate_parallel(model, num_steps=1000):
+    """
+    Evaluate a RL agent
+    :param model: (BaseRLModel object) the RL Agent
+    :param num_steps: (int) number of timesteps to evaluate it
+    :return: (float) Mean reward
+    """
+    episode_rewards = [[0.0] for _ in range(env.num_envs)]
+    obs = env.reset()
+    for i in range(num_steps):
+        # _states are only useful when using LSTM policies
+        actions, _states = model.predict(obs)
+        # here, action, rewards and dones are arrays
+        # because we are using vectorized env
+        obs, rewards, dones, info = env.step(actions)
+
+        # Stats
+        for i in range(env.num_envs):
+            episode_rewards[i][-1] += rewards[i]
+            if dones[i]:
+                episode_rewards[i].append(0.0)
+
+    mean_rewards = [0.0 for _ in range(env.num_envs)]
+    n_episodes = 0
+    for i in range(env.num_envs):
+        mean_rewards[i] = np.mean(episode_rewards[i])
+        n_episodes += len(episode_rewards[i])
+
+        # Compute mean reward
+    mean_reward = round(np.mean(mean_rewards), 1)
+    print("Mean reward:", mean_reward, "Num episodes:", n_episodes)
+
+    return mean_reward
+
+
+def base_env_func():
+    return ValidCardGameAlt()
+
+
+# base_env = ValidCardGame()
+base_env = ValidCardGameAlt()
+num_cpu = 8
 env = DummyVecEnv([lambda: base_env])
-
-model = PPO2(MlpPolicy, env, verbose=1)
-model.learn(total_timesteps=50000)
-
-obs = env.reset()
-total_rewards = 0
-for i in range(20):
-    action, _states = model.predict(obs)
-    new_obs, rewards, dones, info = env.step(action)
-
-    if rewards[0] > 0:
-        hand = [Card.from_number(x) for x in obs[0]]
-        cards = pick_cards_from_hand(action[0], hand)
-        hand_str = Card.display_cards_string(obs[0])
-        card_str = ' '.join(str(x) for x in cards)
-        print("hand: {:s} selected: {:s} reward: {:f}".format(hand_str, card_str, rewards[0]))
-
-    obs = new_obs
-    total_rewards += rewards[0]
-    # env.render()
+# env = SubprocVecEnv([base_env_func for i in range(num_cpu)])
+agent = PPO2(MlpPolicy, env, verbose=1, learning_rate=0.01)
+agent.learn(total_timesteps=1000000)
 
 
-print("total rewards: ", total_rewards)
-# env.env_method("display_past_result")
+# evaluate(model, env)
+evaluate_alt(agent, env)
+# evaluate_parallel(agent)
+
+
+
+
