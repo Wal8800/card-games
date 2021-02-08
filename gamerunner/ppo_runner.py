2cf622c1e191cca77d43b73fcb051b5343f0f455
John <johnlaw8800@gmail.com>
2021-02-08 17:16:12 +1300

    Use a private package for agent

--- gamerunner/ppo_runner.py
+++ gamerunner/ppo_runner.py
@@ -1,130 +1,18 @@
 import logging
 import sys
+import time
 from collections import Counter
 from typing import Dict, List
-import time
 
 import numpy as np
-import scipy.signal
-import scipy.signal
-import tensorflow as tf
-import tensorflow.keras as keras
-from tensorflow.keras import layers, optimizers, models
-from tensorflow_probability import distributions as tfd
+import seaborn as sns
+from algorithm.agent import PPOAgent, discounted_sum_of_rewards, get_mlp_vf, get_mlp_policy
 
 from bigtwo import BigTwo
 from bigtwo.bigtwo import BigTwoObservation
 from playingcards.card import Card
 
 FORMATTER = logging.Formatter("%(asctime)s — %(name)s — %(levelname)s — %(message)s")
-
-
-def discounted_sum_of_rewards(rewards, gamma):
-    """
-    [1 2 3]
-
-    [
-      0.99^0 * 1 + 0.99^1 * 2 + 0.99^2 * 3
-      0.99^0 * 2 + 0.99^1 * 3
-      0.99^0 * 3
-    ]
-    """
-    return scipy.signal.lfilter([1], [1, float(-gamma)], rewards[::-1], axis=0)[::-1]
-
-
-class PPOAgent:
-    def __init__(self, obs_shape, act_dim, env_name: str, dir_path: str = None):
-        self.obs_shape = obs_shape
-        self.act_dim = act_dim
-        self.env_name = env_name
-        self.train_v_iters = 80
-        self.train_p_iters = 80
-        self.target_kl = 0.01
-        self.clip_ratio = 0.3
-
-        # create policy and value function from saved files.
-        if dir_path is not None:
-            custom_obj = {"PPO": PPO}
-            self.policy = models.load_model(f"{dir_path}/{self.env_name}_policy.h5", custom_objects=custom_obj)
-            self.value_function = models.load_model(f"{dir_path}/{self.env_name}_value_function.h5",
-                                                    custom_objects=custom_obj)
-            return
-
-        # creating the policy
-        obs_inp = layers.Input(shape=obs_shape, name="obs")
-        x = layers.Dense(256, activation="relu")(obs_inp)
-        x = layers.Dense(256, activation="relu")(x)
-        output = layers.Dense(act_dim)(x)
-        self.policy = PPO(clip_ratio=self.clip_ratio, inputs=obs_inp, outputs=output)
-        self.policy.compile(optimizer=optimizers.Adam(learning_rate=0.01))
-
-        value_input = layers.Input(shape=obs_shape, name="obs")
-        x = layers.Dense(256, activation="relu")(value_input)
-        x = layers.Dense(256, activation="relu")(x)
-        value_output = layers.Dense(1)(x)
-        self.value_function = keras.Model(inputs=value_input, outputs=value_output)
-        self.value_function.compile(optimizer=optimizers.Adam(0.01), loss=keras.losses.MeanSquaredError())
-
-    def action(self, observation):
-        wrapped = np.array([observation])
-        dist = tfd.Categorical(logits=self.policy(wrapped))
-        sampled_action = dist.sample().numpy()
-        return sampled_action[0], dist.log_prob(sampled_action).numpy()[0]
-
-    def update(self, buf):
-        obs_buf, act_buf, adv_buf, ret_buf, logp_buf = buf.get()
-
-        # update the value function
-        for _ in range(self.train_v_iters):
-            value_loss = self.value_function.train_step((obs_buf, tf.constant(ret_buf)))
-
-        # update the policy by a single step
-        for i in range(self.train_p_iters):
-            result = self.policy.train_step([obs_buf, act_buf, adv_buf, logp_buf])
-            kl = result["approx_kl"]
-            if kl > 1.5 * self.target_kl:
-                print(f"Early stopping at step {i} due to reaching max kl: {kl}")
-                break
-
-        return result["loss"].numpy(), value_loss['loss'].numpy()
-
-    def predict_value(self, observations):
-        return self.value_function.predict(observations)
-
-    def save(self, dir_path: str):
-        self.policy.save(f"{dir_path}/{self.env_name}_policy.h5")
-        self.value_function.save(f"{dir_path}/{self.env_name}_value_function.h5")
-
-
-class PPO(keras.Model):
-    def __init__(self, clip_ratio=0.2, **kwargs):
-        self.clip_ratio = clip_ratio
-        super(PPO, self).__init__(**kwargs)
-
-    def train_step(self, data):
-        batch_obs, batch_acts, batch_adv, previous_log_prob = data
-
-        with tf.GradientTape() as tape:
-            result = self(batch_obs, training=True)
-            curr_log_probs = tfd.Categorical(logits=result).log_prob(batch_acts)
-
-            policy_ratio = tf.exp(curr_log_probs - previous_log_prob) * batch_adv
-            epsilon = tf.constant(self.clip_ratio)
-            limit = tf.where(batch_adv > 0, (1 + epsilon) * batch_adv, (1 - epsilon) * batch_adv)
-            policy_changes = tf.math.minimum(policy_ratio, limit)
-
-            approx_kl = tf.reduce_mean(previous_log_prob - curr_log_probs)
-
-            loss = -tf.reduce_mean(policy_changes)
-
-            # Compute gradients
-            trainable_vars = self.trainable_variables
-            gradient = tape.gradient(loss, trainable_vars)
-
-            # Update weights
-            self.optimizer.apply_gradients(zip(gradient, trainable_vars))
-
-        return {"loss": loss, "approx_kl": approx_kl}
 
 
 def obs_to_np_array(obs: BigTwoObservation) -> np.ndarray:
@@ -236,7 +124,24 @@ def get_logger(logger_name, log_level):
     return logger
 
 
-def train(batch_size=8000, epoch=50):
+def save_prob_plot(obs: BigTwoObservation, action_cat_mapping, bot, filename: str) -> None:
+    prob_frequency = {}
+    total_prob = 0
+    obs_array = obs_to_np_array(obs)
+    for action_cat, raw_action in action_cat_mapping.items():
+        num_of_cards = sum(raw_action)
+
+        probs = bot.prob(obs_array, action_cat)
+
+        prob_frequency[num_of_cards] = prob_frequency.get(num_of_cards, 0) + probs
+        total_prob += probs
+
+    ax = sns.barplot(x=list(prob_frequency.keys()), y=list(prob_frequency.values()))
+    figure = ax.get_figure()
+    figure.savefig(f"./plots/{filename}.png", dpi=400)
+
+
+def train(batch_size=4000, epoch=50):
     train_logger = get_logger("train_ppo", log_level=logging.INFO)
     env = BigTwo()
 
@@ -244,24 +149,35 @@ def train(batch_size=8000, epoch=50):
     result = obs_to_np_array(obs)
 
     # numer of possible actions picking combination from 13 cards.
-    num_of_actions = 2 ** 13
+    total_num_of_actions = 2 ** 13
+    action_cat_mapping = {}
+    cat = 0
+    for i in range(total_num_of_actions):
+        action = int_to_binary_array(i)
 
-    action_cat_mapping = {i: int_to_binary_array(i) for i in range(num_of_actions)}
+        if sum(action) > 5:
+            continue
 
-    bot = PPOAgent(result.shape, num_of_actions, "BigTwo")
+        if sum(action) == 3 or sum(action) == 4:
+            continue
+
+        action_cat_mapping[cat] = action
+        cat += 1
+
+    bot = PPOAgent(get_mlp_policy(result.shape, len(action_cat_mapping.keys())), get_mlp_vf(result.shape), "BigTwo",
+                   policy_lr=0.001, value_lr=0.001)
+
     episode_step = 0
     for i_episode in range(epoch):
         # make some empty lists for logging.
-        batch_lens = []  # for measuring episode lengths
-        batch_rets = []
-        batch_hands_played = []
-        batch_games_played = 0
+        batch_lens, batch_rets, batch_hands_played, batch_games_played = [], [], [], 0  # for measuring episode lengths
         env.reset()
 
         player_buf = create_player_buf()
         player_ep_rews = create_player_rew_buf()
-        num_of_cards_played = []
+        num_of_cards_played, game_history = [], []
         buf = GameBuffer()
+        sample_start_time = time.time()
         for t in range(batch_size):
             obs = env.get_current_player_obs()
 
@@ -293,20 +209,21 @@ def train(batch_size=8000, epoch=50):
 
             epoch_ended = t == batch_size - 1
             if done or epoch_ended:
-                # use the game winning path to log
-                ep_rews = player_ep_rews[obs.current_player]
-                ep_ret, ep_len = sum(ep_rews), len(ep_rews)
                 ep_turns = env.get_hands_played()
-                batch_rets.append(ep_ret)
-                batch_lens.append(ep_len)
                 batch_hands_played.append(ep_turns)
 
                 if done:
                     batch_games_played += 1
+                game_history.append(env.state)
 
+                ep_ret, ep_len = 0, 0
                 # add to buf
                 # process each player buf then add it to the big one
                 for player_number in range(4):
+                    ep_rews = player_ep_rews[player_number]
+                    ep_ret += sum(ep_rews)
+                    ep_len += len(ep_rews)
+
                     if player_buf[player_number].is_empty():
                         continue
 
@@ -319,25 +236,40 @@ def train(batch_size=8000, epoch=50):
                     player_buf[player_number].finish_path(estimated_values, last_val)
                     buf.add(player_buf[player_number])
 
+                batch_rets.append(ep_ret)
+                batch_lens.append(ep_len)
+
                 # reset game
-                env.reset()
+                reset_obs = env.reset()
                 player_buf = create_player_buf()
                 player_ep_rews = create_player_rew_buf()
 
+                if epoch_ended and i_episode % 10 == 0:
+                    save_prob_plot(reset_obs, action_cat_mapping, bot, f"epoch_{i_episode}_ended_reset_probs")
+
+        sample_time_taken = time.time() - sample_start_time
+
+        update_start_time = time.time()
         policy_loss, value_loss = bot.update(buf)
 
-        total_rewards_mean = np.mean(batch_rets)
+        update_time_taken = time.time() - update_start_time
+
         epoch_summary = f"epoch: {i_episode + 1}, policy_loss: {policy_loss:.3f}, " \
                         f"value_loss: {value_loss:.3f}, " \
-                        f"return: {total_rewards_mean:.3f}, " \
+                        f"return: {np.mean(batch_rets):.3f}, " \
                         f"ep_len: {np.mean(batch_lens):.3f} " \
                         f"ep_hands_played: {np.mean(batch_hands_played):.3f} " \
-                        f"ep_games_played: {batch_games_played}"
+                        f"ep_games_played: {batch_games_played} " \
+                        f"time to sample: {sample_time_taken} " \
+                        f"time to update network: {update_time_taken}"
         train_logger.info(epoch_summary)
         train_logger.info(f"num_of_cards_played_summary: {Counter(num_of_cards_played).most_common()}")
+
+        for game in game_history[:5]:
+            train_logger.info(game)
 
 
 if __name__ == '__main__':
     start_time = time.time()
-    train()
+    train(epoch=100)
     print(f"Time taken: {time.time() - start_time:.3f} seconds")
